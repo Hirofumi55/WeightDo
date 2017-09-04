4702ed25d87cc6971ce257bf701ff31dad0249aa
Hirofumi <sasa1235711@gmail.com>
2017-09-05 00:20:35 +0900

    TableViewをお試し実装

--- WeightDo/WeightDo/ViewController.swift
+++ WeightDo/WeightDo/ViewController.swift
@@ -8,10 +8,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet weak var tableView: UITableView!
-
+    
+    let sample: [String]  = ["Swift", "Java", "Python", "JavaScript"]
+    
+    let weightMenu: [String] = []
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -20,11 +26,33 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         
+    }
+    
+    //セルに表示する文字列の個数を返す
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return weightMenu.count
+    }
+    
+    //セルに値を設定するデータソースメソッド
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        // セルを取得する
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "WeightCell")
+        
+        // セルに表示する値を設定する
+        cell.textLabel?.text = weightMenu[indexPath.row]
+        
+        return cell
+    }
+    
+    
+    //セルがタップされた時に呼び出されるデリゲートメソッド
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
     }
     
     
     
-
+    
 }
 
