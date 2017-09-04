35272b40bf3d727ca0b192f7b04e7576ff0840f6
Hirofumi <sasa1235711@gmail.com>
2017-09-05 02:21:14 +0900

    RealmController作成

--- WeightDo/WeightDo/RealmController.swift
+++ WeightDo/WeightDo/RealmController.swift
@@ -13,7 +13,7 @@ import RealmSwift
 class WeightData: Object {
     
     //トレーニングメニュー名
-    dynamic var weightName: String = "none"
+    dynamic var name: String = "none"
     
     //セット数
     dynamic var set: String = "1"
@@ -23,6 +23,65 @@ class WeightData: Object {
     
     //説明
     dynamic var option: String? = ""
+    
+    
+}
+
+class RealmController {
+    
+    //シングルトンの実装
+    static let sharedInstance = RealmController()
+    
+    //デフォルトイニシャライザ
+    init() {
+        
+    }
+    
+    //RealmのWeightDataを取得するメソッド
+    func getWeightData() -> Results<WeightData> {
+        let realm = try! Realm()
+        let result = realm.objects(WeightData)
+        
+        return result
+    }
+    
+    //RealmにWeightDataを追記するメソッド
+    func addWeightData(name: String, set: String, rep: String, option: String) -> () {
+        let realm = try! Realm()
+        let weightData = WeightData()
+        
+        //インスタンスに値を代入する
+        weightData.name = name
+        weightData.set = set
+        weightData.rep = rep
+        weightData.option = option
+        
+        try! realm.write {
+            realm.add(weightData)
+        }
+        
+    }
+    
+    //Realmに指定したWeightDataを削除
+    func deleteWeightData(weightData: WeightData) -> () {
+        let realm = try! Realm()
+        
+        try! realm.write {
+            realm.delete(weightData)
+        }
+    }
+    
+    
+    //Realmのデータをすべて削除する
+    //テスト用などの機能であり通常使用しないこと
+    func allDeleteWeightData() -> () {
+        let realm = try! Realm()
+        
+        try! realm.write {
+            realm.deleteAll()
+        }
+    }
+    
     
     
 }
