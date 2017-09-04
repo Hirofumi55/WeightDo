9aa73cf6a505541352852dc86461ad8ee18a293e
Hirofumi <sasa1235711@gmail.com>
2017-09-04 23:02:42 +0900

    Project Start

---
+++ WeightDo/WeightDo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WeightDo
+//
+//  Created by Sasakura Hirofumi on 2017/09/04.
+//  Copyright © 2017 Sasakura Hirofumi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
