41f7adf55edd12631c981aaf4fb3d902b1b59131
Hirofumi <sasa1235711@gmail.com>
2017-09-06 02:00:31 +0900

    色々修正

--- WeightDo/WeightDo/ViewController.swift
+++ WeightDo/WeightDo/ViewController.swift
@@ -7,30 +7,42 @@
 //
 
 import UIKit
+import RealmSwift
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
+    
     @IBOutlet weak var tableView: UITableView!
     
-    let sample: [String]  = ["Swift", "Java", "Python", "JavaScript"]
+    //let sample: [String]  = ["Swift", "Java", "Python", "JavaScript"]
     
-    let weightMenu: [String] = []
-    
-    
+    //tableView読み込み
+    var result: Results<WeightData>?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let realm = try! Realm()
+        result = realm.objects(WeightData.self)
+        tableView.reloadData()
     }
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        tableView.reloadData()
+    }
+    
+    //画面が表示される時などに再読み込みする
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         
     }
     
+    
     //セルに表示する文字列の個数を返す
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return weightMenu.count
+        return result!.count
     }
     
     //セルに値を設定するデータソースメソッド
@@ -39,8 +51,10 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         // セルを取得する
         let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "WeightCell")
         
+        let object = result?[indexPath.row]
+        
         // セルに表示する値を設定する
-        cell.textLabel?.text = weightMenu[indexPath.row]
+        cell.textLabel?.text = object?.name
         
         return cell
     }
@@ -49,6 +63,15 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     //セルがタップされた時に呼び出されるデリゲートメソッド
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
+    }
+    
+    //Addボタンタップ
+    @IBAction func tapAddButton(_ sender: Any) {
+        
+    }
+    
+    //Composeボタンタップ
+    @IBAction func tapComposeButton(_ sender: Any) {
     }
     
     
